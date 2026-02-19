9ea7821490d704d1aaaf346912d6c357f73d3774
hem.c <dropmoon3523@gmail.com>
2026-02-19 23:06:58 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,16 +5,9 @@ const urlsToCache = [
   "/manifest.json"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", e => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener("fetch", e => {});
+
